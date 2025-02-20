1d80adfb261953ed45d811388c462fd7abe7aef7
AngelGZA <angelalfonso109@gmail.com>
2025-02-20 01:31:47 -0600

    Practica 8 completada con exito.

--- practicas/p08/set_producto_v2.php
+++ practicas/p08/set_producto_v2.php
@@ -31,11 +31,15 @@ if ($stmt_check->num_rows > 0) {
 }
 $stmt_check->close();
 
-$sql_insert = "INSERT INTO productos 
+/*$sql_insert = "INSERT INTO productos 
                VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, 0)";
 $stmt_insert = $link->prepare($sql_insert);
 $stmt_insert->bind_param('sssdsis', $nombre, $marca, $modelo, $precio, $detalles, $unidades, $imagen);
-
+*/
+$sql_insert = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) 
+               VALUES (?, ?, ?, ?, ?, ?, ?)";
+$stmt_insert = $link->prepare($sql_insert);
+$stmt_insert->bind_param('sssdsis', $nombre, $marca, $modelo, $precio, $detalles, $unidades, $imagen);
 
 
 if ($stmt_insert->execute()) {
